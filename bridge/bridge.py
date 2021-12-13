3d5a54a5556a46ed957857ed8905692498bfbf6b
Paula K <paula.klinke@student.hpi.de>
2021-12-13 16:35:24 +0100

    Change differentiation between platforms and arduinos

--- bridge/bridge.py
+++ bridge/bridge.py
@@ -33,7 +33,7 @@ class Bridge():
         for port in ports:
             print (port.device)
             print (port.description)
-            if (platform == 'linux' and 'seeeduino' in port.description.lower()) or (platform == 'win32' and 'com4' in port.description.lower()) or ("usb" in port.description.lower()) or ("leonardo" in port.description.lower()):
+            if (platform == 'linux' and ('seeeduino' or 'usb' or 'leonardo') in port.description.lower()) or (platform == 'win32' and 'com4' in port.description.lower()):
                 self.portname = port.device
                 print ("connecting to " + self.portname)
                 break
