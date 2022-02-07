ffb6f66d6454029d1dc46a8a3ac4d562871834b0
Stefano Lugli <stoppy98@gmail.com>
2022-02-07 23:14:56 +0000

    More flake8 fixes

--- bridge/data/data.py
+++ bridge/data/data.py
@@ -1,20 +1,20 @@
 
 class DataSet():
-	def __init__(self, sensorid):
-		self.sensorid = sensorid
-		self.data = []
+    def __init__(self, sensorid):
+        self.sensorid = sensorid
+        self.data = []
 
-	def datasize(self):
-		return len(self.data)
+    def datasize(self):
+        return len(self.data)
 
-	def addValue(self, value):
-		self.data.append(value)
+    def addValue(self, value):
+        self.data.append(value)
 
-	def getJSON(self, bridge_name):
-		# creating json like data
-		data = {}
-		data['bridgeid'] = str(bridge_name)
-		data['sensorid'] = str(self.sensorid)
-		data['datasize'] = str(self.datasize())
-		data['data'] = [str(datapoint) for datapoint in self.data]
-		return data
+    def getJSON(self, bridge_name):
+        # creating json like data
+        data = {}
+        data['bridgeid'] = str(bridge_name)
+        data['sensorid'] = str(self.sensorid)
+        data['datasize'] = str(self.datasize())
+        data['data'] = [str(datapoint) for datapoint in self.data]
+        return data
--- bridge/data/protocol.py
+++ bridge/data/protocol.py
@@ -1,10 +1,13 @@
 import types
 
+
 def emptyProtocolInBuffer():
-    return types.SimpleNamespace(data = [], flags = -1, device_id = -1, data_length = 0)
+    return types.SimpleNamespace(data=[], flags=-1, device_id=-1, data_length=0)
+
 
 class ProtocolValsIterator:
     """ Useful for iterating over the values inside this buffer """
+
     def __init__(self, buff):
         self._buffer = buff
         self._index = 0
@@ -19,7 +22,7 @@ class ProtocolValsIterator:
 
 class ProtocolBuffer:
 
-    def __init__(self, beginTrigger = b'\xff', endTrigger = b'\xfe'):
+    def __init__(self, beginTrigger=b'\xff', endTrigger=b'\xfe'):
         self.inBuffer = emptyProtocolInBuffer()
         self.beginTrigger = beginTrigger
         self.endTrigger = endTrigger
@@ -30,7 +33,7 @@ class ProtocolBuffer:
         needs to be implemented better!
         """
 
-        if val == self.endTrigger: #EOLi
+        if val == self.endTrigger:  # EOLi
             return True
 
         elif val == self.beginTrigger:
@@ -47,7 +50,7 @@ class ProtocolBuffer:
             else:
                 self.addValue(val)
 
-            if self.inBuffer.data_length > 250: #EOL
+            if self.inBuffer.data_length > 250:  # EOL
                 return True
 
             else:
@@ -71,7 +74,7 @@ class ProtocolBuffer:
 
         elif val == self.endTrigger:
             excceptionString = "Cannot add " + self.endTrigger + " value within data section"
-            raise Exception(exceptionString)
+            raise Exception(excceptionString)
 
         elif self.inBuffer.data_length > 250:
             raise Exception("Adding too many bytes to the data section!")
@@ -82,12 +85,12 @@ class ProtocolBuffer:
 
     def setInitializationFlag(self, boolean):
         if self.inBuffer.flags & (1 << 7) == 128:  # Checking if it was already set
-            if boolean == True:                    # If yes leave as is
+            if boolean:                    # If yes leave as is
                 return
             else:
                 self.inBuffer.flags -= 128          # Else take off the flag
         else:
-            if boolean == True:                    # Same here but inverted
+            if boolean:                    # Same here but inverted
                 self.inBuffer.flags += 128
             else:
                 return
@@ -98,15 +101,14 @@ class ProtocolBuffer:
         else:
             return False
 
-
     def setDebugFlag(self, boolean):
         if self.inBuffer.flags & (1 << 6) == 64:  # Checking if it was already set
-            if boolean == True:                    # If yes leave as is
+            if boolean:                    # If yes leave as is
                 return
             else:
                 self.inBuffer.flags -= 64          # Else take off the flag
         else:
-            if boolean == True:                    # Same here but inverted
+            if boolean:                    # Same here but inverted
                 self.inBuffer.flags += 64
             else:
                 return
@@ -117,24 +119,23 @@ class ProtocolBuffer:
         else:
             return False
 
-
-    def setActuatorFlag(self,boolean):
+    def setActuatorFlag(self, boolean):
         if self.inBuffer.flags & (1 << 5) == 32:  # Checking if it was already set
-            if boolean == True:                    # If yes leave as is
+            if boolean:                    # If yes leave as is
                 return
             else:
                 self.inBuffer.flags -= 32          # Else take off the flag
         else:
-            if boolean == True:                    # Same here but inverted
+            if boolean:                    # Same here but inverted
                 self.inBuffer.flags += 32
             else:
                 return
+
     def isActuatorMessage(self):
         if self.inBuffer.flags & (1 << 5) == 32:
             return True
         else:
             return False
-
 
     def setDeviceId(self, device_id):
         self.inBuffer.device_id = device_id
@@ -148,16 +149,16 @@ class ProtocolBuffer:
     def getDataAsList(self):
         return self.inBuffer.data
 
-    def getDataAsString(self, debug = False):
+    def getDataAsString(self, debug=False):
         message = ""
         for i in range(0, len(self.inBuffer.data)):
-            if self.inBuffer.data[i] == 0: #in this case it's a 0 ending string, so it actually finishes here!
+            if self.inBuffer.data[i] == 0:  # in this case it's a 0 ending string, so it actually finishes here!
                 return message
             try:
                 message += self.inBuffer.data[i].decode("ascii")
             except:
-                if debug == True:
-                    print("Wrong character for:",i,self.inBuffer.data[i])
+                if debug:
+                    print("Wrong character for:", i, self.inBuffer.data[i])
         return message
 
     def flushBuffer(self):
@@ -181,9 +182,9 @@ class ProtocolBuffer:
 
     def toString(self):
         message = "Flags active: \n\r"
-        message += "\t - Debug: " + str(self.isDebugMessage()) +"\n\r"
-        message += "\t - Actuator: " + str(self.isInitializationMessage()) +"\n\r"
-        message += "\t - Initialization: " + str(self.isActuatorMessage()) +"\n\r"
+        message += "\t - Debug: " + str(self.isDebugMessage()) + "\n\r"
+        message += "\t - Actuator: " + str(self.isInitializationMessage()) + "\n\r"
+        message += "\t - Initialization: " + str(self.isActuatorMessage()) + "\n\r"
         message += "Device ID: " + str(self.getDeviceId()) + "\n\r"
         message += "Data Length: " + str(self.getDataSize()) + "\n\r"
         message += "Data: " + str(self.getDataAsList()) + "\n\r"
@@ -195,10 +196,12 @@ class ProtocolBuffer:
         else:
             return None
 
+
 class InitializationBuffer(ProtocolBuffer):
     def __init__(self):
         super().__init__()
-    def getDataType():
+
+    def getDataType(self):
         return self.gedDataAsString()
 
 
@@ -206,7 +209,7 @@ if __name__ == "__main__":
     buff = ProtocolBuffer()
     print(buff.toString())
     buff.readChar(b'\xff')
-    buff.readChar(128+32)
+    buff.readChar(128 + 32)
     buff.readChar(10)
     buff.readChar(5)
     buff.readChar(b'c')
